f106a2ede0771e7bdc81ca51bbdbc6f91ad40dc2
Jarle Holtan <jarle.holtan@bekk.no>
2025-03-27 21:54:33 +0100

    feat: add logging to hello endpoint

--- src/main/kotlin/no/bekk/demoapp/hello/HelloController.kt
+++ src/main/kotlin/no/bekk/demoapp/hello/HelloController.kt
@@ -1,12 +1,18 @@
 package no.bekk.demoapp.hello
 
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HelloController {
+  private val logger = LoggerFactory.getLogger(this::class.java)
+
   @GetMapping("/", produces = ["application/json"])
-  fun index() = Hello(text = "Hello World")
+  fun index(): Hello {
+    logger.warn("Calling index")
+    return Hello(text = "Hello World")
+  }
 
   data class Hello(
     val text: String,
